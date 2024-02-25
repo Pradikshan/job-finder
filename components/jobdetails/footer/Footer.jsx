50bfdca8393ac65d593f01771fed813b32791218
Pradikshan <pradikshan234@gmail.com>
2024-02-25 16:11:17 +0530

    Updated component

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, Image, Linking } from 'react-native';
 
 import styles from './footer.style';
+import { icons } from '../../../constants';
 
-const Footer = () => {
+const Footer = ({ url }) => {
   return (
-    <View>
-      <Text>Footer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <Image
+          source={icons.heartOutline}
+          resizeMode='contain'
+          style={styles.likeBtnImage}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
