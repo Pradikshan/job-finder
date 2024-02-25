3978c2ceb25ea51ab44b593f389ea4960b97a6e1
Pradikshan <pradikshan234@gmail.com>
2024-02-25 16:20:35 +0530

    Made minor syntax changes

--- app/search/[id].js
+++ app/search/[id].js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native'
-import { Stack, useRouter, useSearchParams } from 'expo-router'
-import { Text, SafeAreaView } from 'react-native'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { Stack, useRouter, useSearchParams } from 'expo-router';
+import { Text, SafeAreaView } from 'react-native';
+import axios from 'axios';
 
-import { ScreenHeaderBtn, NearbyJobCard } from '../../components'
-import { COLORS, icons, SIZES } from '../../constants'
-import styles from '../../styles/search'
+import { ScreenHeaderBtn, NearbyJobCard } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import styles from '../../styles/search';
 
 const JobSearch = () => {
     const params = useSearchParams();
@@ -130,7 +130,7 @@ const JobSearch = () => {
                 )}
             />
         </SafeAreaView>
-    )
+    );
 }
 
-export default JobSearch
+export default JobSearch;
