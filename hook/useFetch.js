f3519b67bfd02b6d56fb8f2875d9f78dde940b3c
Pradikshan <pradikshan234@gmail.com>
2024-02-25 10:22:31 +0530

    Moved header to the top

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -9,14 +9,15 @@ const useFetch = (endpoint) => {
     const options = {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        headers: {
+            'X-RapidAPI-Key': 'adf2c48dc7mshde4ae60fcaaca5ap1320f7jsne0b438a31ab4',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        },
         params: {
           query: 'Python developer in Texas, USA',
           page: '1',
           num_pages: '1'
         },
-        headers: {
-          'X-RapidAPI-Key': 'adf2c48dc7mshde4ae60fcaaca5ap1320f7jsne0b438a31ab4',
-          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        }
+
     };
 }
