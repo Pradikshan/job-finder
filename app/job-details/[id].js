0db87bab11795cc73e503e86eae17bc9e409a9a8
Pradikshan <pradikshan234@gmail.com>
2024-02-25 11:56:25 +0530

    Added local imports

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -4,7 +4,9 @@ import {
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { useCallback, useState } from "react";
 
-
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn } from '../../components';
+import { COLORS, icons, SIZES } from '../../constants';
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
   return (
