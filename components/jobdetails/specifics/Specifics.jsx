475bab7e97f41f0dad2a59249699fc262083de92
Pradikshan <pradikshan234@gmail.com>
2024-02-25 15:32:08 +0530

    Made minor syntax changes

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
 
-import styles from './specifics.style'
+import styles from './specifics.style';
 
 const Specifics = () => {
   return (
     <View>
       <Text>Specifics</Text>
     </View>
-  )
+  );
 }
 
-export default Specifics
+export default Specifics;
