f7e14cbaf609bcf4c062b141815400ab1aa6789e
Pradikshan <pradikshan234@gmail.com>
2024-02-25 11:53:06 +0530

    Made minor changes

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,9 @@
-import React from 'react';
+import { Text } from "react-native";
 
 const JobDetails = () => {
   return (
-    <div>JobDetails</div>
-  )
+    
+  );
 }
 
 export default JobDetails;
