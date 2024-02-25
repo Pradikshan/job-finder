d9319e9e3fcc523dfb11b5b2141f5b3fbc845e96
Pradikshan <pradikshan234@gmail.com>
2024-02-25 11:44:06 +0530

    Refactored code

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -7,12 +7,12 @@ import { checkImageURL } from '../../../../utils';
 const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
     <TouchableOpacity
-      style={styles.container(selectedJob, item)}
-      onPress={() => handleCardPress(item)}
+      style={styles.container}
+      onPress={handleNavigate}
     >
-      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+      <TouchableOpacity style={styles.logoContainer}>
         <Image  
-          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg' }}
+          source={{ uri: checkImageURL(job.employer_logo) ? job.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg' }}
           resizeMode='contain'
           style={styles.logoImage}
         />
