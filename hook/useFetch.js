4ed345b8185fd5d742308555598923b34f7967d2
Pradikshan <pradikshan234@gmail.com>
2024-02-25 10:15:24 +0530

    Set up the states

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,1 +1,8 @@
 import { useState, useEffect } from "react";
+import axios from'axios';
+
+const useFetch = () => {
+    const [data, useData] = useData([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+}
