cf5add850e13f2b7675c354433daf8912590defa
Pradikshan <pradikshan234@gmail.com>
2024-02-25 10:25:18 +0530

    Import API key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from'axios';
+import { RAPID_API_KEY } from '@env';
+
+const rapidApiKey = RAPID_API_KEY;
 
 const useFetch = (endpoint) => {
     const [data, useData] = useData([]);
@@ -10,14 +13,10 @@ const useFetch = (endpoint) => {
         method: 'GET',
         url: `https://jsearch.p.rapidapi.com/${endpoint}`,
         headers: {
-            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Key': rapidApiKey,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'
-        },
+        params: {...query },
 
     };
 }
