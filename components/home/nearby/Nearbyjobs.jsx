043a18eb5dc648ad10c6485be132a77c5926abbf
Pradikshan <pradikshan234@gmail.com>
2024-02-25 11:34:08 +0530

    Removed unused import

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { 
-  View, Text, TouchableOpacity, FlatList, ActivityIndicator 
+  View, Text, TouchableOpacity, ActivityIndicator 
 } from 'react-native';
 import { useRouter } from 'expo-router';
 
