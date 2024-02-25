3f65e2469d3efe7e74350cdeb8fb41de6acd6760
Pradikshan <pradikshan234@gmail.com>
2024-02-25 11:42:27 +0530

    Refactored code

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,50 +1,37 @@
-import { 
-  View, Text, TouchableOpacity, ActivityIndicator 
-} from 'react-native';
-import { useRouter } from 'expo-router';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-import styles from './nearbyjobs.style';
-import { COLORS } from '../../../constants';
-import NearbyJobCard from '../../common/cards/nearby/NearbyJobCard';
-import useFetch from '../../../hook/useFetch';
+import styles from './nearbyjobcard.style';
 
-const Nearbyjobs = () => {
-  const router = useRouter();
-  const { data, isLoading, error } = useFetch(
-    'search',{
-    query: 'React developer',
-    num_pages: 1
-  });
+import { checkImageURL } from '../../../../utils';
 
-
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Nearby jobs</Text>
-        <TouchableOpacity>
-          <Text style={styles.headerBtn}>Show all</Text>
-        </TouchableOpacity>
-      </View>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image  
+          source={{ uri: checkImageURL(item.employer_logo) ? item.employer_logo : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg' }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
 
-      <View style={styles.cardsContainer}>
-        {isLoading ? (
-          <ActivityIndicator size='large' color={COLORS.primary} />
-        ) : error ? (
-          <Text>Something went wrong</Text>
-        ): (
-          data?.map((job) => (
-            <NearbyJobCard 
-              job={job}
-              key={`nearby-job-${job?.job_id}`}
-              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
-            />
-          ))
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
 
-        )
-        }
+        <Text style={styles.location}>{item.job_country}</Text>
+
       </View>
-    </View>
+    </TouchableOpacity>
+    
   );
-}
+};
 
-export default Nearbyjobs;
+export default NearbyJobCard;
