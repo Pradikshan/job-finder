68b89c11b79ae53793b6445b8d7f5d48eb0d2fca
Pradikshan <pradikshan234@gmail.com>
2024-02-25 10:18:37 +0530

    Added API options code snippet

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,4 +5,18 @@ const useFetch = () => {
     const [data, useData] = useData([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/search`,
+        params: {
+          query: 'Python developer in Texas, USA',
+          page: '1',
+          num_pages: '1'
+        },
+        headers: {
+          'X-RapidAPI-Key': 'adf2c48dc7mshde4ae60fcaaca5ap1320f7jsne0b438a31ab4',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+    };
 }
