c6b7475867a49b1611f12dd672c4f3f13899a5ed
Pradikshan <pradikshan234@gmail.com>
2024-02-25 15:20:41 +0530

    Made minor syntax changes

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
 
-import styles from './tabs.style'
+import styles from './tabs.style';
 
 const Tabs = () => {
   return (
     <View>
       <Text>Tabs</Text>
     </View>
-  )
+  );
 }
 
-export default Tabs
+export default Tabs;
