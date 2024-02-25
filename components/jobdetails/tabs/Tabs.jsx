2902f3460d60bdeafd6f3a01cfd92ef0c6f40687
Pradikshan <pradikshan234@gmail.com>
2024-02-25 15:23:17 +0530

    Updated component

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,11 +1,20 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 
 import styles from './tabs.style';
+import { SIZES } from '../../../constants';
 
-const Tabs = () => {
+
+
+
+const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <View style={styles.container}>
+      <FlatList 
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton  />
+        )}
+      />
     </View>
   );
 }
